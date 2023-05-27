8b84fc7d23c8d0e9ceadea5679d2ee8de59cc48e
watashinokisetsu <134773956+watashinokisetsu@users.noreply.github.com>
2023-05-27 17:08:47 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,17 @@
-document.addEventListener('DOMContentLoaded', function() {
-  var column1Content = document.getElementById('column1-content');
-  var column2Content = document.getElementById('column2-content');
-
-  window.addEventListener('scroll', function() {
-    var scrollPosition = window.pageYOffset || document.documentElement.scrollTop;
-
-    // Определите условия для каждого варианта фона и содержимого столбцов
-    if (scrollPosition < 500) {
-      document.body.className = 'variant1';
-      column1Content.textContent = 'Содержимое столбца 1 - Вариант 1';
-      column2Content.textContent = 'Содержимое столбца 2 - Вариант 1';
-    } else if (scrollPosition >= 500 && scrollPosition < 1000) {
-      document.body.className = 'variant2';
-      column1Content.textContent = 'Содержимое столбца 1 - Вариант 2';
-      column2Content.textContent = 'Содержимое столбца 2 - Вариант 2';
-    } else {
-      document.body.className = 'variant3';
-      column1Content.textContent = 'Содержимое столбца 1 - Вариант 3';
-      column2Content.textContent = 'Содержимое столбца 2 - Вариант 3';
-    }
-  });
-});
+// Изменение содержимого при прокрутке страницы
+    window.addEventListener('scroll', function() {
+      var textGenerationQuery = document.getElementById('text-generation-query');
+      var imageGenerationQuery = document.getElementById('image-generation-query');
+      
+      // Примеры текста
+      if (window.scrollY < 500) {
+        textGenerationQuery.textContent = 'Пример запроса для генерации текста';
+        imageGenerationQuery.textContent = '...';
+      }
+      
+      // Примеры изображений
+      else {
+        textGenerationQuery.textContent = '...';
+        imageGenerationQuery.textContent = 'Пример запроса для генерации изображений';
+      }
+    });
