e265513013376eadeecfd69e3b9a37c5a0738334
watashinokisetsu <134773956+watashinokisetsu@users.noreply.github.com>
2023-05-28 15:47:52 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
-window.addEventListener('scroll', () => {
-    const block1 = document.getElementById('block1').getBoundingClientRect();
-    const block2 = document.getElementById('block2').getBoundingClientRect();
-    const block3 = document.getElementById('block3').getBoundingClientRect();
+function setSectionBackgrounds() {
+  const sections = document.querySelectorAll('.section');
+  const scrollPosition = window.pageYOffset;
 
-    if (block1.top >= 0 && block1.bottom > window.innerHeight / 2) {
-        document.body.style.background = getComputedStyle(document.getElementById('block1')).background;
-    } else if (block2.top >= 0 && block2.bottom > window.innerHeight / 2) {
-        document.body.style.background = getComputedStyle(document.getElementById('block2')).background;
-    } else if (block3.top >= 0) {
-        document.body.style.background = getComputedStyle(document.getElementById('block3')).background;
+  sections.forEach(function(section, index) {
+    const sectionTop = section.offsetTop;
+    const sectionHeight = section.clientHeight;
+
+    if (scrollPosition >= sectionTop - sectionHeight / 3) {
+      section.style.backgroundColor = `rgba(0, 0, 0, ${index / sections.length})`;
     }
-});
+  });
+}
+
+window.addEventListener('scroll', setSectionBackgrounds);
+window.addEventListener('resize', setSectionBackgrounds);
