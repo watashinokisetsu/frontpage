1c7bbc787c86a62ee1c5d44f077dd651daf8cee2
watashinokisetsu <134773956+watashinokisetsu@users.noreply.github.com>
2023-05-28 15:21:29 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 window.addEventListener('scroll', () => {
-    const currentScroll = window.pageYOffset;
-    const block1Height = document.getElementById('block1').offsetHeight;
-    const block2Height = document.getElementById('block2').offsetHeight;
-    const block3Height = document.getElementById('block3').offsetHeight;
+    const block1 = document.getElementById('block1').getBoundingClientRect();
+    const block2 = document.getElementById('block2').getBoundingClientRect();
+    const block3 = document.getElementById('block3').getBoundingClientRect();
 
-    if (currentScroll < block1Height) {
+    if (block1.top >= 0 && block1.bottom > window.innerHeight / 2) {
         document.body.style.background = getComputedStyle(document.getElementById('block1')).background;
-    } else if (currentScroll < block1Height + block2Height) {
+    } else if (block2.top >= 0 && block2.bottom > window.innerHeight / 2) {
         document.body.style.background = getComputedStyle(document.getElementById('block2')).background;
-    } else {
+    } else if (block3.top >= 0) {
         document.body.style.background = getComputedStyle(document.getElementById('block3')).background;
-   
+    }
+});
