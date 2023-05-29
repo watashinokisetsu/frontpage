a1ff2f978be942a5878fab32b69dc3ea915b17ff
watashinokisetsu <134773956+watashinokisetsu@users.noreply.github.com>
2023-05-29 14:05:38 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
+const sections = document.querySelectorAll('section');
+const colors = ['hsl(0, 60%, 70%)', 'hsl(120, 60%, 70%)', 'hsl(240, 60%, 70%)']; // different colors for each section
+
 window.addEventListener("scroll", function() {
-    const currentScroll = window.pageYOffset;
-    const maxScroll = document.body.scrollHeight - window.innerHeight;
+    for (let i = 0; i < sections.length; i++) {
+        const rect = sections[i].getBoundingClientRect();
 
-    const scrollFraction = currentScroll / maxScroll;
-    const hueRotation = scrollFraction * 360;
-
-    document.body.style.backgroundColor = `hsl(${hueRotation}, 60%, 70%)`;
+        if (rect.top < window.innerHeight && rect.bottom > 0) {
+            document.body.style.backgroundColor = colors[i];
+            break;
+        }
+    }
 });
