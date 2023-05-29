f62f5d4f45dcdd6fc5861a9e763baf3a5091e914
watashinokisetsu <134773956+watashinokisetsu@users.noreply.github.com>
2023-05-29 14:23:37 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 const sections = document.querySelectorAll('section');
-const colors = ['hsl(0, 60%, 70%)', 'hsl(120, 60%, 70%)', 'hsl(240, 60%, 70%)']; // different colors for each section
+const classes = ['color1', 'color2', 'color3']; // corresponding classes for each section
 
 window.addEventListener("scroll", function() {
     for (let i = 0; i < sections.length; i++) {
         const rect = sections[i].getBoundingClientRect();
 
         if (rect.top < window.innerHeight && rect.bottom > 0) {
-            document.body.style.backgroundColor = colors[i];
+            document.body.className = classes[i]; // change the class of body
             break;
         }
     }
